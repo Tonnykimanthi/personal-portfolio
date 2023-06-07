c584bb1d27efd75c9c7142ab98190eddee9ebd48
Tony Km <kimaxhanthony9@gmail.com>
2023-06-07 17:20:39 +0300

    Personal portfolio

--- public/script.js
+++ public/script.js
@@ -1,12 +1,22 @@
-const navC = document.querySelector('.nav-for-sm');
-const openNav = document.querySelector('.open-nav-btn');
-const closeNav = document.querySelector('.close-nav-btn');
+const navC = document.querySelector(".nav-for-sm");
+const navListForSmall = document.querySelectorAll(".nav-li-for-sm");
+const openNav = document.querySelector(".open-nav-btn");
+const closeNav = document.querySelector(".close-nav-btn");
 
-openNav.addEventListener('click', ()=>{
-    navC.classList.add('flex', 'min-[500px]:hidden')
-    navC.classList.remove('hidden')
+openNav.addEventListener("click", () => {
+  navC.classList.add("flex", "min-[500px]:hidden");
+  navC.classList.remove("hidden");
 });
-closeNav.addEventListener('click', ()=>{
-    navC.classList.remove('flex', 'min-[500px]:hidden')
-    navC.classList.add('hidden')
+closeNav.addEventListener("click", () => {
+  navC.classList.remove("flex", "min-[500px]:hidden");
+  navC.classList.add("hidden");
+});
+
+navListForSmall.forEach((li) => {
+  li.addEventListener("click", hideNavContainer);
+
+  function hideNavContainer() {
+    navC.classList.remove("flex", "min-[500px]:hidden");
+    navC.classList.add("hidden");
+  }
 });
