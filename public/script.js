747688194895dbe7f1a6e7648b4502a655d73c55
Tony Km <kimaxhanthony9@gmail.com>
2023-06-08 17:25:57 +0300

    Hide Navbar on scroll down and vice versa

--- public/script.js
+++ public/script.js
@@ -3,6 +3,22 @@ const navListForSmall = document.querySelectorAll(".nav-li-for-sm");
 const openNav = document.querySelector(".open-nav-btn");
 const closeNav = document.querySelector(".close-nav-btn");
 
+const mainNav = document.getElementById("header-nav");
+
+let prevScrollPos = window.pageYOffset;
+window.onscroll = () => {
+  let currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    mainNav.classList.toggle('top-0', true);
+    mainNav.classList.toggle('-top-14', false);
+  } else {
+    mainNav.classList.toggle('top-0', false);
+    mainNav.classList.toggle('-top-14', true);
+  }
+  prevScrollPos = currentScrollPos;
+}
+
 openNav.addEventListener("click", () => {
   navC.classList.add("flex", "min-[500px]:hidden");
   navC.classList.remove("hidden");
